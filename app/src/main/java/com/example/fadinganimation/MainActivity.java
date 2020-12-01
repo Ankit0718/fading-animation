e341ceba9b6aa96fe55935a0018470f7287d973c
Ankit kumar <ankitkumar.kumar2000ankit@gmail.com>
2020-12-01 19:16:58 +0530

    fading animation

---
+++ app/src/main/java/com/example/fadinganimation/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fadinganimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+        boolean tomIsShowing = true;
+    public void fade(View view){
+        Log.i("Info","image pressed!");
+        ImageView tomImageView = (ImageView) findViewById(R.id.tomImageView);
+        ImageView jerryImageView = (ImageView) findViewById(R.id.jerryImageView);
+
+        if (tomIsShowing) {
+             tomIsShowing = false;
+            tomImageView.animate().alpha(0).setDuration(2000);
+            jerryImageView.animate().alpha(1).setDuration(2000);
+        }
+        else{
+             tomIsShowing = true;
+            tomImageView.animate().alpha(1).setDuration(2000);
+            jerryImageView.animate().alpha(0).setDuration(2000);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
